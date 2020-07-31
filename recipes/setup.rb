c6b9180e4b49557a6875c2566a52d95a07ab3b1f
vagrant <vagrant@localhost.localdomain>
2020-07-31 23:34:13 +0000

    Initial Workstaion commit

---
+++ recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'nano'
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+package 'ntp'
+
+file '/etc/motd' do
+  content 'This server is the Property of Jose Cedeño'
+  action :create
+  owner 'root'
+  group 'root'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
